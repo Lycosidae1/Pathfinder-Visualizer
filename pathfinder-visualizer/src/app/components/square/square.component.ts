e1bd52337317dc23272958a322c05801e0f7def7
lpdetilly <louis-philippe.de-tilly@polymtl.ca>
2023-12-19 20:51:58 -0500

    small modifications

--- pathfinder-visualizer/src/app/components/square/square.component.ts
+++ pathfinder-visualizer/src/app/components/square/square.component.ts
@@ -14,39 +14,38 @@ export class SquareComponent implements OnInit {
   private startPositionLocked!: boolean
   currentStyles!: {[klass: string]: any; };
 
-  constructor(private mouseService: MouseService, private boardService: BoardService){
-    this.applyNeutralColor();
-  };
+  constructor(private mouseService: MouseService, private boardService: BoardService) {};
 
   ngOnInit(): void {
+    this.handleSubscriptions();
+    this.applyNeutralColor();
+  }
+
+  handleSubscriptions(): void {
     this.mouseService.mouseState.asObservable().subscribe((state) => {
       this.mouseState = state;
     });
     this.boardService.clearBoardEvent.asObservable().subscribe(() => {
       this.applyNeutralColor();
       this.boardService.unlockStartPosition();
-    })
+    });
     this.boardService.nodeSelection.asObservable().subscribe((selection) => {
       this.nodeSelection = selection
-    })
+    });
     this.boardService.startPositionIsSet.asObservable().subscribe((value) => {
       this.startPositionLocked = value;
-    })
+    });
   }
 
   block(): void {
-    if(this.nodeSelection === NodeSelection.Obstacle) {
-      if(this.mouseState == MouseState.DOWN) {
+    if(this.nodeSelection === NodeSelection.Obstacle && this.mouseState === MouseState.DOWN) {
       this.applyColor();
-      this.state = !this.state;
     }
-    }
-
   }
 
-  onClick(e:any): void {
+  onClick(): void {
     if(this.nodeSelection === NodeSelection.Obstacle) this.handleObstacle();
-    else if(this.nodeSelection === NodeSelection.Start ) this.handleStartPosition();
+    else if(this.nodeSelection === NodeSelection.Start) this.handleStartPosition();
   }
 
   start(): void {
@@ -59,10 +58,8 @@ export class SquareComponent implements OnInit {
     if(this.nodeSelection === NodeSelection.Obstacle) {
       if(this.mouseState == MouseState.DOWN) {
         this.applyColor();
-        this.state = !this.state;
       }
     }
-
   }
 
   startSelection(): void {
@@ -78,18 +75,19 @@ export class SquareComponent implements OnInit {
     this.currentStyles = {
       'background-color': 'black',
     }
+    this.state = !this.state;
   }
 
   applyNeutralColor(): void {
     this.currentStyles = {
       'background-color': 'white',
     }
+    this.state= !this.state;
   }
 
   handleObstacle(): void {
     if(this.currentStyles['background-color'] === 'black') {
       this.applyNeutralColor();
-      this.state = !this.state;
     }
     else {
       this.applyColor();
